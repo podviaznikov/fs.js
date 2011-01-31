c1f881043c883d74b502836c2002c16863e457f6
Anton Podviaznikov <podviaznikov@gmail.com>
2011-01-31 22:25:50 +0100

    updated with one extra demo. More changes to come

---
+++ file.dnd.js
@@ -0,0 +1,18 @@
+"use strict";
+var FileDropZone = function(id,filesHandler)
+{
+    var dropZone = document.getElementById(id);
+    dropZone.addEventListener('dragover', function(evt)
+    {
+        evt.stopPropagation();
+        evt.preventDefault();
+    }, false);
+    dropZone.addEventListener('drop', function(evt)
+    {
+        evt.stopPropagation();
+        evt.preventDefault();
+        filesHandler(evt.dataTransfer.files); // handle FileList object.
+    }, false);
+}
+
+
