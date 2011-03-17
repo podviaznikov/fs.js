2f5dc01a2bf58505e190d4a2ea94da20fbf82123
Anton Podviaznikov <podviaznikov@gmail.com>
2011-03-17 14:51:33 +0000

    continued refactoring

--- fs.io.js
+++ fs.io.js
@@ -13,248 +13,318 @@
  */
 "use strict";
 var global = this;
-fs.io = (function()
+fs.io = Object.create({},
 {
-    return {
 
-        /* READING FILES*/
+    /* READING FILES*/
 
-        /**
-         * Method reads content of the file as dataURL.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
-         * Before using dataURL user should check whether error happened.
-         */
-        readAsDataUrl:function(fileName,callback)
+    /**
+     * Method reads content of the file as dataURL.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
+     * Before using dataURL user should check whether error happened.
+     */
+    readAsDataUrl:
+    {
+        value:function(fileName,callback)
         {
             fs.util.readAsDataUrl(fileName,callback,{});
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the temporary file as dataURL.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
-         * Before using dataURL user should check whether error happened.
-         */
-        readTmpFileAsDataUrl:function(fileName,callback)
+    /**
+     * Method reads content of the temporary file as dataURL.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
+     * Before using dataURL user should check whether error happened.
+     */
+    readTmpFileAsDataUrl:
+    {
+        value:function(fileName,callback)
         {
             fs.util.readAsDataUrl(fileName,callback,{tmp:true});
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the file as plain text.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and text.
-         * Before using text user should check whether error happened.
-         */
-        readAsText:function(fileName,callback)
+    /**
+     * Method reads content of the file as plain text.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and text.
+     * Before using text user should check whether error happened.
+     */
+    readAsText:
+    {
+        value:function(fileName,callback)
         {
             fs.util.readAsText(fileName,callback,{});
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the temporary file as plain text.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and text.
-         * Before using text user should check whether error happened.
-         */
-        readTmpFileAsText:function(fileName,callback)
+    /**
+     * Method reads content of the temporary file as plain text.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and text.
+     * Before using text user should check whether error happened.
+     */
+    readTmpFileAsText:
+    {
+        value:function(fileName,callback)
         {
             fs.util.readAsText(fileName,callback,{tmp:true});
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the file as binary text.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and binary string.
-         * Before using binary string result user should check whether error happened.
-         */
-        readAsBinaryString:function(fileName,callback)
+    /**
+     * Method reads content of the file as binary text.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and binary string.
+     * Before using binary string result user should check whether error happened.
+     */
+    readAsBinaryString:
+    {
+        value:function(fileName,callback)
         {
             fs.util.readAsBinaryString(fileName,callback,{});
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the temporary file as binary text.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and binary string.
-         * Before using binary string result user should check whether error happened.
-         */
-        readTmpFileAsBinaryString:function(fileName,callback)
+    /**
+     * Method reads content of the temporary file as binary text.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and binary string.
+     * Before using binary string result user should check whether error happened.
+     */
+    readTmpFileAsBinaryString:
+    {
+        value:function(fileName,callback)
         {
             fs.util.readAsBinaryString(fileName,callback,{tmp:true});
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the file as array buffer.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
-         * Before using array buffer user should check whether error happened.
-         */
-        readAsArrayBuffer:function(fileName,callback)
+    /**
+     * Method reads content of the file as array buffer.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
+     * Before using array buffer user should check whether error happened.
+     */
+    readAsArrayBuffer:
+    {
+        value:function(fileName,callback)
         {
             fs.util.readAsArrayBuffer(fileName,callback,{});
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the temporary file as array buffer.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
-         * Before using array buffer user should check whether error happened.
-         */
-        readTmpFileAsArrayBuffer:function(fileName,callback)
+    /**
+     * Method reads content of the temporary file as array buffer.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
+     * Before using array buffer user should check whether error happened.
+     */
+    readTmpFileAsArrayBuffer:
+    {
+        value:function(fileName,callback)
         {
             fs.util.readAsArrayBuffer(fileName,callback,{tmp:true});
-        },
+        }
+    },
 
 
-        /**
-         * Method reads content of the file as plain text.
-         * @param file - file object.
-         * @param callback - callback after operation is done. Has 2 parameters: error and text.
-         * Before using text user should check whether error happened.
-         */
-        readFileAsText:function(file,callback)
+    /**
+     * Method reads content of the file as plain text.
+     * @param file - file object.
+     * @param callback - callback after operation is done. Has 2 parameters: error and text.
+     * Before using text user should check whether error happened.
+     */
+    readFileAsText:
+    {
+        value:function(file,callback)
         {
             fs.util.getReader(file,callback,function(reader,theFile)
             {
                 reader.readAsText(theFile);
             });
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the file as dataURL.
-         * @param file - file object.
-         * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
-         * Before using datURL user should check whether error happened.
-         */
-        readFileAsDataURL:function(file,callback)
+    /**
+     * Method reads content of the file as dataURL.
+     * @param file - file object.
+     * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
+     * Before using datURL user should check whether error happened.
+     */
+    readFileAsDataURL:
+    {
+        value:function(file,callback)
         {
             fs.util.getReader(file,callback,function(reader,theFile)
             {
                 reader.readAsDataURL(theFile);
             });
-        },
+        }
+    },
 
-        /**
-         * Method reads content of the file as array buffer.
-         * @param fileName - name of the file in the file system.
-         * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
-         * Before using array buffer user should check whether error happened.
-         */
-        readFileAsArrayBuffer:function(file,callback)
+    /**
+     * Method reads content of the file as array buffer.
+     * @param fileName - name of the file in the file system.
+     * @param callback - callback after operation is done. Has 2 parameters: error and dataURL.
+     * Before using array buffer user should check whether error happened.
+     */
+    readFileAsArrayBuffer:
+    {
+        value:function(file,callback)
         {
             fs.util.readFileAsArrayBuffer(file,callback);
-        },
+        }
+    },
 
-        /* Files and Directories*/
+    /* Files and Directories*/
 
-        createFile:function(fileName,callback)
+    createFile:
+    {
+        value:function(fileName,callback)
         {
             fs.util.createFile(fileName,callback,{});
-        },
-        createTmpFile:function(fileName,callback)
+        }
+    },
+    createTmpFile:
+    {
+        value:function(fileName,callback)
         {
             fs.util.createFile(fileName,callback,{tmp:true});
-        },
+        }
+    },
 
-        createDirectory:function(directoryName,callback)
+    createDirectory:
+    {
+        value:function(directoryName,callback)
         {
             fs.util.createDirectory(directoryName,callback,{});
-        },
+        }
+    },
 
-        createTmpDirectory:function(directoryName,callback)
+    createTmpDirectory:
+    {
+        value:function(directoryName,callback)
         {
             fs.util.createDirectory(directoryName,callback,{tmp:true});
-        },
+        }
+    },
 
-        /* WRITING DATA*/
-        writeFileToFile:function(file,callback)
+    /* WRITING DATA*/
+    writeFileToFile:
+    {
+        value:function(file,callback)
         {
             fs.util.writeFileToFile(file,callback,{});
-        },
+        }
+    },
 
-        writeFileToTmpFile:function(file,callback)
+    writeFileToTmpFile:
+    {
+        value:function(file,callback)
         {
             fs.util.writeFileToFile(file,callback,{tmp:true});
-        },
+        }
+    },
 
-        /**
-         * Method for writing blob to file.
-         *
-         * @param fileName - filename in which data should be written. File will be created.
-         * @param blob - content of the file.
-         * @param callback - callback after execution may contain only one parameter: error.
-         */
-        writeBlobToFile:function(fileName,blob,callback)
+    /**
+     * Method for writing blob to file.
+     *
+     * @param fileName - filename in which data should be written. File will be created.
+     * @param blob - content of the file.
+     * @param callback - callback after execution may contain only one parameter: error.
+     */
+    writeBlobToFile:
+    {
+        value:function(fileName,blob,callback)
         {
             fs.util.writeBlobToFile(fileName,blob,callback,{});
-        },
+        }
+    },
 
-        /**
-         * Method for writing blob to temp file.
-         *
-         * @param fileName - filename in which data should be written. File will be created.
-         * @param blob - content of the file.
-         * @param callback - callback after execution may contain only one parameter: error.
-         */
-        writeBlobToTmpFile:function(fileName,blob,callback)
+    /**
+     * Method for writing blob to temp file.
+     *
+     * @param fileName - filename in which data should be written. File will be created.
+     * @param blob - content of the file.
+     * @param callback - callback after execution may contain only one parameter: error.
+     */
+    writeBlobToTmpFile:
+    {
+        value:function(fileName,blob,callback)
         {
             fs.util.writeBlobToFile(fileName,blob,callback,{tmp:true});
-        },
+        }
+    },
 
-        /**
-         * Method for writing text to the file.
-         *
-         * @param fileName - name of the file. File will be created.
-         * @param text - text (multi line using \r\n) that should be written.
-         * @param callback - callback with error parameter if something went wrong.
-         */
-        writeTextToFile:function(fileName,text,callback)
+    /**
+     * Method for writing text to the file.
+     *
+     * @param fileName - name of the file. File will be created.
+     * @param text - text (multi line using \r\n) that should be written.
+     * @param callback - callback with error parameter if something went wrong.
+     */
+    writeTextToFile:
+    {
+        value:function(fileName,text,callback)
         {
             fs.util.writeTextToFile(fileName,text,callback,{});
-        },
+        }
+    },
 
-        /**
-         * Method for writing text to the temporary file.
-         *
-         * @param fileName - name of the file. File will be created.
-         * @param text - text (multi line using \r\n) that should be written.
-         * @param callback - callback with error parameter if something went wrong.
-         */
-        writeTextToTmpFile:function(fileName,text,callback)
+    /**
+     * Method for writing text to the temporary file.
+     *
+     * @param fileName - name of the file. File will be created.
+     * @param text - text (multi line using \r\n) that should be written.
+     * @param callback - callback with error parameter if something went wrong.
+     */
+    writeTextToTmpFile:
+    {
+        value:function(fileName,text,callback)
         {
             fs.util.writeTextToFile(fileName,text,callback,{tmp:true});
-        },
+        }
+    },
 
-        /**
-         * Method for writing base64 encoded string to file.
-         *
-         * @param fileName - name of the file. File will be created.
-         * @param content - encoded string.
-         * @param contentType - content type.
-         * @param callback - callback with error parameter if something went wrong.
-         */
-        writeBase64StrToFile:function(fileName,content,contentType,callback)
+    /**
+     * Method for writing base64 encoded string to file.
+     *
+     * @param fileName - name of the file. File will be created.
+     * @param content - encoded string.
+     * @param contentType - content type.
+     * @param callback - callback with error parameter if something went wrong.
+     */
+    writeBase64StrToFile:
+    {
+        value:function(fileName,content,contentType,callback)
         {
             fs.util.writeBase64StrToFile(fileName,content,contentType,callback,{});
-        },
+        }
+    },
 
-         /**
-         * Method for writing base64 encoded string to temp file.
-         *
-         * @param fileName - name of the file. File will be created.
-         * @param content - encoded string.
-         * @param contentType - content type.
-         * @param callback - callback with error parameter if something went wrong.
-         */
-        writeBase64StrToTmpFile:function(fileName,content,contentType,callback)
+     /**
+     * Method for writing base64 encoded string to temp file.
+     *
+     * @param fileName - name of the file. File will be created.
+     * @param content - encoded string.
+     * @param contentType - content type.
+     * @param callback - callback with error parameter if something went wrong.
+     */
+    writeBase64StrToTmpFile:
+    {
+        value:function(fileName,content,contentType,callback)
         {
             fs.util.writeBase64StrToFile(fileName,content,contentType,callback,{tmp:true});
-        },
+        }
+    },
 
-        /*Directories*/
-        readDirectory:function(directoryName,callback)
+    /*Directories*/
+    readDirectory:
+    {
+        value:function(directoryName,callback)
         {
             fs.util.getDirectory(directoryName,function(err,directory)
             {
@@ -267,9 +337,12 @@ fs.io = (function()
                     fs.util.readEntriesFromDirectory(directory,callback);
                 }
             },{});
-        },
+        }
+    },
 
-        readRootDirectory:function(callback)
+    readRootDirectory:
+    {
+        value:function(callback)
         {
             fs.getNativeFS(function(err,filesystem)
             {
@@ -283,6 +356,6 @@ fs.io = (function()
                 }
             },{});
         }
-    };
-})();
+    }
+});
 
